a42a909d58a5a499db65431e4157ffffa8448fa6
Tashakashkina <tashakashkina@gmail.com>
2022-11-12 21:58:26 +0300

    finished app structure: routes&factory

---
+++ lib/application/ui/screen_factory/screen_factory.dart
@@ -0,0 +1,6 @@
+import 'package:e_commerce/application/ui/screens/main_tabs/main_tabs_screen.dart';
+import 'package:flutter/material.dart';
+
+class ScreenFactory {
+  Widget makeMainTabs() => const MainTabsScreen();
+}
---
+++ lib/application/ui/screens/main_tabs/main_tabs_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainTabsScreen extends StatelessWidget {
+  const MainTabsScreen ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold();
+    
+  }
+}

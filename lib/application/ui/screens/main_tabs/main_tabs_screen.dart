7f6394402e800b241ca95ae452d2f34be7bd0382
Tashakashkina <tashakashkina@gmail.com>
2022-11-19 21:44:12 +0300

    icons color switching&slivers

--- lib/application/ui/screens/main_tabs/main_tabs_screen.dart
+++ lib/application/ui/screens/main_tabs/main_tabs_screen.dart
@@ -8,16 +8,37 @@ class MainTabsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      
-       bottomNavigationBar: SizedBox(height: 72,  
-          child:
-        _NavBarWidget()
+ 
+    return const Scaffold(
+      body: _BodyWidget(),
+      bottomNavigationBar: SizedBox(height: 72,  
+          child:_NavBarWidget()
     ),);
   }
 }
 
+class _BodyWidget extends StatelessWidget {
+  const _BodyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     final currentIndex = context.select(
+    (MainTabsViewModel vm) => vm.currentTabIndex
+  );
+  return IndexedStack(
+        index: currentIndex,
+        children: [
+        Center(
+          child: Text('1')),
+          Center(
+          child: Text('2')),
+          Center(
+          child: Text('3')),
+          Center(
+          child: Text('4')),
+      ]);
+  }
+}
 class _NavBarWidget extends StatelessWidget {
   const _NavBarWidget({
     Key? key,
@@ -28,6 +49,17 @@ class _NavBarWidget extends StatelessWidget {
   final currentIndex = context.select(
     (MainTabsViewModel vm) => vm.currentTabIndex
   );
+
+  final theme = Theme.of(context).bottomNavigationBarTheme;
+  final buttons = [
+    _BottomNavigationBarItemFactory('assets/icons/1/tabbar_explorer.png', 'Explorer'),
+    _BottomNavigationBarItemFactory('assets/icons/1/tabbar_bag.png', 'Корзина'),
+    _BottomNavigationBarItemFactory('assets/icons/1/tabbar_heart.png', 'Избранное'),
+    _BottomNavigationBarItemFactory('assets/icons/1/tabbar_person.png', 'Личный кабинет')
+  ].asMap().map((index, value) {
+    return MapEntry(index, value.build(index, currentIndex, theme));
+  }).values.toList();
+
   final model = context.read<MainTabsViewModel>();
     return BottomNavigationBar(
       showSelectedLabels: true,
@@ -36,23 +68,23 @@ class _NavBarWidget extends StatelessWidget {
       onTap: model.setCurrentTabIndex,
       type: BottomNavigationBarType.fixed,
       
-      items: [
-        
-         BottomNavigationBarItem(icon: Image.asset('assets/icons/1/tabbar_explorer.png'),
-         label: '',
-         tooltip: 'Explorer'
-      ),
-        BottomNavigationBarItem(icon: Image.asset('assets/icons/1/tabbar_bag.png'),  
-        label: '',
-        tooltip: 'Корзина'),
-        BottomNavigationBarItem(icon: Image.asset('assets/icons/1/tabbar_heart.png'), 
-        label: '',
-        tooltip: 'Избранное'),
-        BottomNavigationBarItem(icon: Image.asset('assets/icons/1/tabbar_person.png'), 
-        label: '',
-        tooltip: 'Личный кабинет'),
-      ], 
-      
-    );
+      items: buttons);
   }
 }
+  
+class _BottomNavigationBarItemFactory {
+  final String iconName;
+  final String tooltip;
+
+  _BottomNavigationBarItemFactory (this.iconName, this.tooltip);
+
+  BottomNavigationBarItem build(
+    int index, int currentIndex, BottomNavigationBarThemeData theme){
+     final color = index == currentIndex ? theme.selectedItemColor : theme.unselectedItemColor;
+      return  BottomNavigationBarItem(
+         label: '',     
+         icon: Image.asset( iconName,
+         color: color),
+         tooltip: tooltip,
+      );
+}}
---
+++ lib/application/ui/screens/main_tabs/schedule_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Schedule_Widget extends StatelessWidget {
+  const Schedule_Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+    
+  }
+}
